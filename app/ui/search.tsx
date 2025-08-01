7ea61455546ae35e74274e9e1f3ba67d8af91dce
HinanJiHyun <wlgusdldi15@naver.com>
2025-08-01 09:09:38 +0900

    invoices list, pagination study

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,16 +1,25 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams } from 'next/navigation';
-
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import {useDebouncedCallback} from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
+  const pathname = usePathname(); //현재경로
+  const {replace} = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching for ${term}...`);
     const params = new URLSearchParams(searchParams);
-    console.log(term);
-  }
+    params.set('page', '1');
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300)
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -23,6 +32,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
